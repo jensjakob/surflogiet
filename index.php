27331129977683205cd6902ed9ad0e9f075940b9
jensjakob <jensjakob@users.noreply.github.com>
2019-03-21 09:39:56 +0100

    black arrow

--- index.php
+++ index.php
@@ -184,7 +184,7 @@
 			</div>
 			<div class="layer4">
 				<a class="skip-to-content" href="#content">
-					<svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 24 24"><path d="M16.59 8.59L12 13.17 7.41 8.59 6 10l6 6 6-6z" fill="white"/><path d="M0 0h24v24H0z" fill="none"/></svg>
+					<svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 24 24"><path d="M16.59 8.59L12 13.17 7.41 8.59 6 10l6 6 6-6z" fill="black"/><path d="M0 0h24v24H0z" fill="none"/></svg>
 				</a>
 			</div>
 		</div>
