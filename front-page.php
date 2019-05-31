f0573431ce01b2ba82ada44b87c816e229478169
jensjakob <jensjakob@users.noreply.github.com>
2019-05-31 17:42:09 +0200

    switch to english notification

--- front-page.php
+++ front-page.php
@@ -229,6 +229,8 @@ endif;
 	}
 	a {
 		color: black;
+		text-decoration: underline;
+		cursor: pointer;
 	}
 	.go {
 		margin-left: -4px;
@@ -328,6 +330,11 @@ endif;
 	}
 	</style>
 	<div id="content">
+		<div class="hide infobox userlang">
+			<div class="text">
+			<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M0 0h24v24H0z" fill="none"></path><path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-1 17.93c-3.95-.49-7-3.85-7-7.93 0-.62.08-1.21.21-1.79L9 15v1c0 1.1.9 2 2 2v1.93zm6.9-2.54c-.26-.81-1-1.39-1.9-1.39h-1v-3c0-.55-.45-1-1-1H8v-2h2c.55 0 1-.45 1-1V7h2c1.1 0 2-.9 2-2v-.41c2.93 1.19 5 4.06 5 7.41 0 2.08-.8 3.97-2.1 5.39z"></path></svg><p>Hey! Do you prefer English? <a class="switch-language">Switch language instantly</a>.</p>
+			</div>
+		</div>
 		<div id="logiet" class="divider">
 			<div class="layer1"> </div>
 			<div class="layer2">
@@ -757,9 +764,17 @@ Tack!</textarea>
   crossorigin="anonymous"></script>
 
 <script>
+	var userLang = navigator.language || navigator.userLanguage;
+	userLang = userLang.split('-')[0];
+
+	if(userLang != 'sv') {
+		$(".userlang").removeClass('hide');
+	}
+
 	$(".switch-language").click(function() {
 		$("body").toggleClass("lang-sv lang-en");
 	});
+
 	$(".sidebar-open").click(function() {
 		$("#sidebar").toggle();
 	});
